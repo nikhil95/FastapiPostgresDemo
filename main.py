a5c32245946ce643767378a700d673245ad48f5e
nikhil95 <105496233+nikhil95@users.noreply.github.com>
2023-01-06 15:05:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,32 @@ from typing import Union
 
 from fastapi import FastAPI
 
+host_server = os.environ.get('host_server', 'localhost')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
+database_name = os.environ.get('database_name', 'fastapi')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+projects = sqlalchemy.Table(
+    "projects",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(
+    #DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, pool_size=3, max_overflow=0
+)
+metadata.create_all(engine)
+
 app = FastAPI()
 
 
