425b0351d92f1555c44fc8701716fb63fd39dad3
nikhil95 <105496233+nikhil95@users.noreply.github.com>
2023-01-06 16:47:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/ITEMS/{item_id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
